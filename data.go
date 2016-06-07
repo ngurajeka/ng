bfc8a5cb5ef7717c2bb5a7f09ce9b61175866b61
Ady Rahmat MA <ngurajeka.92@gmail.com>
2016-06-08 00:43:59 +0700

    Add more property on data

--- data.go
+++ data.go
@@ -2,6 +2,8 @@ package ng
 
 //Data standard structure of data from jsonapi.org
 type Data struct {
-	Data  interface{} `json:"data"`
-	Links interface{} `json:"links"`
+	Data     interface{} `json:"data"`
+	Included interface{} `json:"included,omitempty"`
+	Links    interface{} `json:"links"`
+	Meta     interface{} `json:"meta,omitempty"`
 }
